c447ba1187f456b5ef5a14ec6b61ec52cf8751d0
meno1011 <39692905+meno1011@users.noreply.github.com>
2022-02-25 11:06:50 +0900

    carousel 4가지 만듬

--- src/components/CarouselBasic.js
+++ src/components/CarouselBasic.js
@@ -1,41 +1,198 @@
-import React from "react";
+import React, { useRef } from "react";
 import Slider from "react-slick";
 import cat1 from "./image/cat1.jpg";
 import cat2 from "./image/cat2.jpg";
 import cat3 from "./image/cat3.jpg";
 import cat4 from "./image/cat4.jpg";
-import './Carousel.css'
+import "./Carousel.css";
 
 const CarouselBasic = () => {
-    var settings = {
-        dots: true
-      };
   return (
     <div>
       <h1>CarouselBasic</h1>
-      {/* <img alt="" src={cat1} /> */}
-      <div className="wrapper">
-        <Slider {...settings}>
-          <div>
-            <img alt="" src={cat1} />
-            <p className="legend" align="center">Legend 1</p>
-          </div>
-          <div>
-            <img alt="" src={cat2} />
-            <p className="legend" align="center">Legend 2</p>
-          </div>
-          <div>
-            <img alt="" src={cat3} />
-            <p className="legend" align="center">Legend 3</p>
-          </div>
-          <div>
-            <img alt="" src={cat4} />
-            <p className="legend" align="center">Legend 4</p>
-          </div>
-        </Slider>
+      <div style={{ display: "flex" }}>
+        <SimpleSlider />
+        <MultiSlider />
+      </div>
+      <div style={{ display: "flex", marginTop:10 }}>
+        <AutoSlider />
+        <ButtonSlider />
       </div>
     </div>
   );
 };
 
 export default CarouselBasic;
+
+const SimpleSlider = () => {
+  const simpleSettings = {
+    dots: true,
+    speed: 500,
+  };
+  return (
+    <div className="wrapper">
+      <Slider {...simpleSettings}>
+        <div>
+          <img alt="" src={cat1} />
+          <p className="legend" align="center">
+            cat 1
+          </p>
+        </div>
+        <div>
+          <img alt="" src={cat2} />
+          <p className="legend" align="center">
+            cat 2
+          </p>
+        </div>
+        <div>
+          <img alt="" src={cat3} />
+          <p className="legend" align="center">
+            cat 3
+          </p>
+        </div>
+        <div>
+          <img alt="" src={cat4} />
+          <p className="legend" align="center">
+            cat 4
+          </p>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+const MultiSlider = () => {
+  const multiSettings = {
+    dots: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+  };
+  return (
+    <div className="wrapper2">
+      <Slider {...multiSettings}>
+        <div>
+          <img alt="" src={cat1} />
+          <p className="legend" align="center">
+            cat 1
+          </p>
+        </div>
+        <div>
+          <img alt="" src={cat2} />
+          <p className="legend" align="center">
+            cat 2
+          </p>
+        </div>
+        <div>
+          <img alt="" src={cat3} />
+          <p className="legend" align="center">
+            cat 3
+          </p>
+        </div>
+        <div>
+          <img alt="" src={cat4} />
+          <p className="legend" align="center">
+            cat 4
+          </p>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+const AutoSlider = () => {
+  const autoSettings = {
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed: 3000,
+    cssEase: "linear"
+  };
+  return (
+    <div className="wrapper">
+      <Slider {...autoSettings}>
+        <div>
+          <img alt="" src={cat1} />
+          <p className="legend" align="center">
+            cat 1
+          </p>
+        </div>
+        <div>
+          <img alt="" src={cat2} />
+          <p className="legend" align="center">
+            cat 2
+          </p>
+        </div>
+        <div>
+          <img alt="" src={cat3} />
+          <p className="legend" align="center">
+            cat 3
+          </p>
+        </div>
+        <div>
+          <img alt="" src={cat4} />
+          <p className="legend" align="center">
+            cat 4
+          </p>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+const ButtonSlider = () => {
+  const slider = useRef();
+  const next =()=> {
+    slider.current.slickNext();
+  }
+  const previous = ()=> {
+    slider.current.slickPrev();
+  }
+  const buttonSettings = {
+    dots: true,
+    speed: 500,
+  };
+  return (
+    <div style={{display:'flex',flexDirection:'column', margin:'0 auto'}}>
+    <div className="wrapper">
+      <Slider ref={slider} {...buttonSettings}>
+        <div>
+          <img alt="" src={cat1} />
+          <p className="legend" align="center">
+            cat 1
+          </p>
+        </div>
+        <div>
+          <img alt="" src={cat2} />
+          <p className="legend" align="center">
+            cat 2
+          </p>
+        </div>
+        <div>
+          <img alt="" src={cat3} />
+          <p className="legend" align="center">
+            cat 3
+          </p>
+        </div>
+        <div>
+          <img alt="" src={cat4} />
+          <p className="legend" align="center">
+            cat 4
+          </p>
+        </div>
+      </Slider>
+    </div>
+      <div style={{ textAlign: "center" }}>
+          <button className="button" onClick={previous}>
+            Previous
+          </button>
+          <button className="button" onClick={next}>
+            Next
+          </button>
+        </div>
+        </div>
+  );
+};
