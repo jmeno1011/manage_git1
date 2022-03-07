b03a261c42a42e3c8abb1dddcc33fff1b104d715
jmeno1011 <jwhltn8282@gmail.com>
2022-03-07 23:32:52 +0900

    clipboard 약간 수정 paste아직 해결 못함

--- src/components/ClipBoard.js
+++ src/components/ClipBoard.js
@@ -1,27 +1,29 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Icon } from "@iconify/react";
 import "./ClipBoard.css";
 import { useNavigate } from "react-router-dom";
 
 const ClipBoard = () => {
   const navigate = useNavigate();
-  const [initext, setInitext] = useState("복사 할 내용");
-  const copy = () => {
-    navigator.clipboard.writeText(initext);
-    alert("복사되었습니다.");
-  };
-  const onClickInsert = (e) => {
-    console.log(
-      navigator.clipboard.addEventListener("paste", (e) => {
-        const paste ='pasted:\n' +
-        (e.clipboardData || window.clipboardData).getData('text');
-    
-      e.target.value = paste;
+  const pre_text = "복사 할 내용 1";
+  const [initext, setInitext] = useState("복사 할 내용 2");
+  const [visual, setVisual] = useState(false);
+  const [visual2, setVisual2] = useState(false);
 
-      // stop default paste
-      e.preventDefault();
-      })
-    );
+  const copy = (i) => {
+    if (i === 0) {
+      navigator.clipboard.writeText(pre_text);
+      setVisual(true);
+      setTimeout(() => {
+        setVisual(false);
+      }, 1000);
+    } else if (i === 1) {
+      navigator.clipboard.writeText(initext);
+      setVisual2(true);
+      setTimeout(() => {
+        setVisual2(false);
+      }, 1000);
+    }
   };
   return (
     <div>
@@ -31,12 +33,32 @@ const ClipBoard = () => {
       </header>
 
       <div className="copy-area">
-        <pre className="copy-area-pre">{initext}</pre>
-        <div className="copy-icon-wrapper" onClick={copy}>
+        <pre className="copy-area-pre">{pre_text}</pre>
+        <div className="copy-icon-wrapper" onClick={() => copy(0)}>
+          <Icon
+            icon="fluent:copy-16-regular"
+            style={{ fontSize: 16 }}
+            name="copy1"
+          />
+        </div>
+        <span className={`alert-text ${visual ? "" : "hidden"}`}>Copied!</span>
+      </div>
+      <div className="copy-area">
+        <input
+          type={"text"}
+          className="copy-area-input"
+          value={initext}
+          onChange={(e) => {
+            setInitext(e.target.value);
+          }}
+        />
+        <div className="copy-icon-wrapper" onClick={() => copy(1)}>
           <Icon icon="fluent:copy-16-regular" style={{ fontSize: 16 }} />
         </div>
+        <span className={`alert-text2 ${visual2 ? "" : "hidden"}`}>
+          Copied!
+        </span>
       </div>
-      <button onClick={onClickInsert}>버튼</button>
     </div>
   );
 };
