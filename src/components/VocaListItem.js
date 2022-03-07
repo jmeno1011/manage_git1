d7c0dba4a419330da136850594a5b8aff3b7178e
jmeno1011 <jwhltn8282@gmail.com>
2022-03-08 00:38:45 +0900

    spread api설정 했으니 추가해보기

--- src/components/VocaListItem.js
+++ src/components/VocaListItem.js
@@ -1,69 +1,73 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import { Icon } from '@iconify/react';
-import './vocaListItem.css'
-import axios from 'axios';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Icon } from "@iconify/react";
+import "./vocaListItem.css";
+import axios from "axios";
 import { useEffect } from "react";
 
 const VocaListItem = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  //   const { data, loading, error } = useGoogleSheets({
+  //     apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  //     sheetId: process.env.REACT_APP_GOOGLE_SHEETS_ID,
+  //   });
 
-    const fetch = async()=>{
-        try{
-            const res = await axios.get('https://docs.google.com/spreadsheets/d/1JutZS1OQ6f5O7fDpctGGNKoF3Hi-wAQtUTcb375RkYo/edit#gid=0')
-            console.log(res);
-            return res
-        }catch{
+  const fetch = async () => {
+    try {
+      const res = await axios.get(
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vTZGWwvVmCADcl0PANk9FE364lSw1xZIVHkS5QIJIj6SG_VgLdUHsRa4MTmvs5jAMhah7EN9CEh-Jvw/pubhtml"
+      );
+      console.log(res);
+      return res;
+    } catch {}
+  };
 
-        }
+  useEffect(() => {
+    fetch();
+  }, []);
 
-        
-    }
-    
-    useEffect(()=>{
-        fetch()
-    },[])
-    
   return (
-    <div >
-        <header>
+    <div>
+      <header>
         <h1>Carousel</h1>
         <button onClick={() => navigate(-1)}>Go back</button>
       </header>
-      <div style={{width:530}}>
-        <div className='vocaItem'>
-            <div className='itemWord'>
-                <div className='wrapWord'>
-                    <div>
-                        <a target={"_blank"} className="word">voca</a>
-                    </div>
-                </div>
-                <div className='wrapBtn'>
-                    <button className='btnListen'>
-                    <Icon icon="clarity:volume-up-line" />
-                    </button>
-                </div>
+      <div style={{ width: 530 }}>
+        <div className="vocaItem">
+          <div className="itemWord">
+            <div className="wrapWord">
+              <div>
+                <a target={"_blank"} className="word">
+                  voca
+                </a>
+              </div>
             </div>
-            <div className='itemMean'>
-                <div className='contentMean'>
-                    <ul className='listMean'>
-                        <li className='wrapmean'>
-                            <div className='meanDesc'>
-                                <span className='num'>1.</span>
-                                <p className='content'>보카 테스트</p>
-                            </div>
-                        </li>
-                    </ul>
-                    <div className='originSource'>
-                        <span className='blind'>출처</span>
-                        해커스 토익
-                    </div>
-                </div>
+            <div className="wrapBtn">
+              <button className="btnListen">
+                <Icon icon="clarity:volume-up-line" />
+              </button>
             </div>
+          </div>
+          <div className="itemMean">
+            <div className="contentMean">
+              <ul className="listMean">
+                <li className="wrapmean">
+                  <div className="meanDesc">
+                    <span className="num">1.</span>
+                    <p className="content">보카 테스트</p>
+                  </div>
+                </li>
+              </ul>
+              <div className="originSource">
+                <span className="blind">출처</span>
+                해커스 토익
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default VocaListItem
+export default VocaListItem;
