d1b585ac1899b02c6d557b7930ac3cf83293b679
meno1011 <39692905+meno1011@users.noreply.github.com>
2022-03-08 16:49:23 +0900

    pagination 공부해서 추가 하기

--- src/components/ClipBoard.js
+++ src/components/ClipBoard.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { Icon } from "@iconify/react";
 import "./ClipBoard.css";
 import { useNavigate } from "react-router-dom";
--- src/components/VocaListItem.js
+++ src/components/VocaListItem.js
@@ -4,6 +4,7 @@ import { Icon } from "@iconify/react";
 import "./vocaListItem.css";
 import axios from "axios";
 import { useEffect } from "react";
+import vocaData from "../json/toeic_voca_2021.json";
 
 const VocaListItem = () => {
   const navigate = useNavigate();
@@ -17,57 +18,72 @@ const VocaListItem = () => {
       const res = await axios.get(
         "https://docs.google.com/spreadsheets/d/e/2PACX-1vTZGWwvVmCADcl0PANk9FE364lSw1xZIVHkS5QIJIj6SG_VgLdUHsRa4MTmvs5jAMhah7EN9CEh-Jvw/pubhtml"
       );
-      console.log(res);
+      //   console.log(res);
       return res;
     } catch {}
   };
 
   useEffect(() => {
     fetch();
+    // console.log(vocaData);
   }, []);
 
   return (
     <div>
       <header>
-        <h1>Carousel</h1>
+        <h1>Voca</h1>
         <button onClick={() => navigate(-1)}>Go back</button>
       </header>
-      <div style={{ width: 530 }}>
-        <div className="vocaItem">
-          <div className="itemWord">
-            <div className="wrapWord">
-              <div>
-                <a target={"_blank"} className="word">
-                  voca
-                </a>
-              </div>
-            </div>
-            <div className="wrapBtn">
-              <button className="btnListen">
-                <Icon icon="clarity:volume-up-line" />
-              </button>
-            </div>
-          </div>
-          <div className="itemMean">
-            <div className="contentMean">
-              <ul className="listMean">
-                <li className="wrapmean">
-                  <div className="meanDesc">
-                    <span className="num">1.</span>
-                    <p className="content">보카 테스트</p>
-                  </div>
-                </li>
-              </ul>
-              <div className="originSource">
-                <span className="blind">출처</span>
-                해커스 토익
-              </div>
-            </div>
-          </div>
-        </div>
+      <div
+        style={{ height: 500, width: 550, margin: "1rem", overflow: "auto" }}
+      >
+        <VocaItem />
       </div>
     </div>
   );
 };
 
 export default VocaListItem;
+
+const VocaItem = () => {
+  return (
+    <>
+      {vocaData.map((value) => (
+        <div style={{ width: 530 }} key={value.index}>
+          <div className="vocaItem">
+            <div className="itemWord">
+              <div className="wrapWord">
+                <div>
+                  <a target={"_blank"} className="word">
+                    {value.words}
+                  </a>
+                </div>
+              </div>
+              <div className="wrapBtn">
+                <button className="btnListen">
+                  <Icon icon="clarity:volume-up-line" />
+                </button>
+              </div>
+            </div>
+            <div className="itemMean">
+              <div className="contentMean">
+                <ul className="listMean">
+                  <li className="wrapmean">
+                    <div className="meanDesc">
+                      <span className="num">{value.index}.</span>
+                      <p className="content">{value.means}</p>
+                    </div>
+                  </li>
+                </ul>
+                <div className="originSource">
+                  <span className="blind">출처</span>
+                  해커스 토익
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
