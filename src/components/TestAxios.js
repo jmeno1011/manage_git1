53075a0ca3c0f4074b8cc7156ef5f4e0a366621b
jmeno1011 <jwhltn8282@gmail.com>
2022-04-07 00:50:27 +0900

    bar-chart 보여주기

--- src/components/TestAxios.js
+++ src/components/TestAxios.js
@@ -5,12 +5,24 @@ import { useNavigate } from "react-router-dom";
 const TestAxios = () => {
   const navigate = useNavigate();
   const [data, setData] = useState();
+  const [barData, setBarData] = useState([]);
+  const showBarData = async () => {
+    try {
+      const response = await axios.get(
+        "https://railway-node-express-production.up.railway.app/bar-chart"
+      );
+      console.log("barData::", response.data);
+      setBarData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
   const fetch = async () => {
     try {
       const response = await axios.get(
         "https://node-express-two.vercel.app/test-api"
       );
-      console.log(response.data);
+      console.log("vercel--test-api::", response.data);
       setData(response.data);
     } catch (e) {
       console.log(e);
@@ -30,6 +42,14 @@ const TestAxios = () => {
         <h2>{data && data.status}</h2>
         <h2>{data && data.message}</h2>
         <hr />
+        <button onClick={showBarData}>show barchart data</button>
+        {barData.map((value) => (
+          <div key={value.City}>
+            <p>City :: {value.City}</p>
+            <p>Population2010 :: {value.Population2010}</p>
+            <p>Population2000 :: {value.Population2000}</p>
+          </div>
+        ))}
       </div>
     </>
   );
