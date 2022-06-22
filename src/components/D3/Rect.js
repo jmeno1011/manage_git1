a059d527a10193e744941dfa4b416c43dd89e98a
jmeno1011 <jwhltn8282@gmail.com>
2022-06-23 00:20:30 +0900

    add rect component

--- src/components/D3/Rect.js
+++ src/components/D3/Rect.js
@@ -1,7 +1,13 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
 
 const Rect = () => {
   const svgRef = useRef();
+  const rectData = [10, 20, 30, 40, 50];
+  useEffect(() => {
+    const svg = d3.select(svgRef);
+    // svg.append("g").selectAll("rect").data(rectData).enter().append("rect");
+  }, []);
   return (
     <>
       <h3>Rect</h3>
